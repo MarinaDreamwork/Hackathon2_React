608ed761438c4b6e346aee092324f78140a9dedb
Nikit099 <nechit1337@gmail.com>
2022-07-09 15:07:36 +0300

    made progress

--- src/app/pages/main.jsx
+++ src/app/pages/main.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Progress from "../components/common/progress";
+
+
 
 const Main = () => {
-  return <h2 className="text-center">Главная страница</h2>;
+
+
+    const [test, setTest] = useState([
+      {bgColor: "#00695c", completed: 57, type:  "bar", id: new Date().getMilliseconds() * Math.random(), label: "HTML" }
+    ]);
+    
+    const changeSelect = ({target}) => {
+      setTest( [{
+        ...test[0], 
+      type: target.value
+    }]);
+    };
+
+  return (
+  <div>
+      <h2 className="text-center">Главная страница</h2>
+      {
+        test.map(i => <Progress changeSelect = {changeSelect} key={i.id} bgColor={i.bgColor} completed={i.completed} label={i.label} type={i.type}/>)
+      }
+  </div>
+  ) ;
 };
 
 export default Main;
